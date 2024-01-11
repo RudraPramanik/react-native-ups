e485e30a1f0855b2b4d4ce771d656893d1a8267e
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-01-11 22:53:04 +0600

    final

--- App.tsx
+++ App.tsx
@@ -1,27 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwind-rn';
-import { useTailwind } from 'tailwind-rn';
-import utilities from './tailwind.json';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './navigator/RootNavigator';
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import SplashScreen from 'react-native-splash-screen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
-    //@ts-ignore - TailwindProvider missing a type defination
-    <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </TailwindProvider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
